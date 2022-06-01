9a312a666101574fe224bf876b3e8909b7d334a7
chefboyronron <rgpseminiano@gmail.com>
2022-06-01 10:53:06 +0800

    chore: add empty data display

--- src/components/FilterContent.js
+++ src/components/FilterContent.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import styles from '../styles/FilterContent.module.css';
-import { Row } from 'antd';
+import { Row, Empty } from 'antd';
 import PersonItem from './PersonItem';
 
 function FilterContent(prop) {
+    console.log(prop.data.length);
+
+    let persons = '';
+
+    if( prop.data.length > 0 ) {
+        persons = prop.data.map((item) => {
+            return <PersonItem item={item} key={item.id}/>
+        })
+    } else {
+        persons = <Empty className='m-auto' image={Empty.PRESENTED_IMAGE_SIMPLE} />
+    }
+
     return(
         <section className={styles['content-wrapper']}>
             <Row>
-                {
-                    prop.data.map((item) => {
-                        return <PersonItem item={item} key={item.id}/>
-                    })
-                }
+                { persons }
             </Row>
         </section>
     )
